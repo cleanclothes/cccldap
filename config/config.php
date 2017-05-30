e7aa0acb176582f87a2de25a84d485512bdcbbe0
Paul Roeland <paul@cleanclothes.org>
2017-05-30 15:41:12 +0200

    set defaults

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
-$cccldap['emailsuffix'] = 'mycompany.org';
-$cccldap['domain'] = 'mydomain.mycompany.org';
-$cccldap['ldaptype'] = 1;
-$cccldap['ldapserver'] = 'pdc.mycompany.org';
+$cccldap['emailsuffix'] = 'cleanclothes.org';
+$cccldap['domain'] = 'ldap.ccc.local';
+$cccldap['ldaptype'] = 2;
+$cccldap['ldapserver'] = 'ldap.ccc.local';
 $cccldap['port'] = '389';
-$cccldap['basedn']= 'CN=users, DC=mydomain,DC=mycompany,DC=org';
+$cccldap['basedn']= 'ou=insite,dc=cleanclothes,dc=org';
 $cccldap['loginfield'] = 'uid';
 $cccldap['usersuffix'] = '.LDAP';
 $cccldap['createusers'] = true;
-$cccldap['email_attribute'] = "userprincipalname";
+$cccldap['email_attribute'] = "mail";
 $cccldap['create_new_match_email'] = false;
 $cccldap['allow_duplicate_email'] = true;
 $cccldap['notification_email'] = "";
